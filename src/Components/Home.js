5bb96495c22ada589ea6079fcbcd575aa0f6b871
ybuchi <yabuch_ch@hotmail.com>
2022-04-09 14:21:27 -0400

    Basic layout being constructed on the Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import "./Images/mountain.jpg"
+import Jumbotron from "./Jumbotron";
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 // What do we want on our home page?
 // Introductory Jumbotron
@@ -18,9 +22,21 @@ function Home(){
 
 
     return(
-       <div className="jumbotron">
-           
-       </div>
+        // Have the "Hi, Bonjour, Hallo" placed horizontally from each other and some animation that boldens each of them one at a time
+       <Jumbotron>
+           <Container>
+               <Row>
+                   <Col md={6}>
+                       Hi!
+                       Bonjour!
+                       Hallo!
+                   </Col>
+                   <Col md={6}>
+                       My name is Yannik and I'm a Mission-Driven Software Engineer
+                   </Col>
+               </Row>
+           </Container>
+       </Jumbotron>
     )
 }
 
