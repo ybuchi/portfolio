82b25120e1dbfea481b376d91388cdb11a33f9aa
ybuchi <yabuch_ch@hotmail.com>
2022-04-10 12:32:06 -0400

    Fixed and centered carousel on Portfolio page

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { portfolio } from "../Data/data";
+import "./ProjectCard.css"
 
 import Carousel from 'react-bootstrap/Carousel';
 
@@ -7,17 +8,17 @@ function ProjectCard (){
     const mappedPortfolio = portfolio.map((project)=>{
         return (
             <Carousel.Item >
-                <img className="w-100" src={require("./Images/mountain.jpg")} alt="moutain"/>
+                <img  style={{width: "100%"}}src={require("./Images/mountain.jpg")} alt="moutain"/>
                 <Carousel.Caption>
                     <h1>{project.project_name}</h1>
-                    <h1>{project.summary}</h1>
+                    <h2>{project.summary}</h2>
                     <h3>{project.tools}</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             )
     })
     return(
-        <Carousel>
+        <Carousel id="project-carousel">
             {mappedPortfolio}
         </Carousel>
         
