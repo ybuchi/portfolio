aad9ae55e75ac491b19c36a33de4cfb28ceb6dc6
ybuchi <yabuch_ch@hotmail.com>
2022-08-05 20:29:38 -0400

    Started making the home page responsive to mobile.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -24,8 +24,6 @@ function Home(){
 
     return(
         <>
-        {/* <img id="logo" src={require("./Images/logo_cropped.png")} alt="Yannik's logo" />*/}
-
             <Jumbotron id="home-jumbo" jumboStyle={jumboStyle} backgroundImage={backgroundImage}>
                 <Container>
                     <Row>
@@ -33,31 +31,27 @@ function Home(){
                             <Row>
                                 <Col md={12} style={{textAlign: "center"}}>
                                     <div>
-                                        {/* <span className="greetings">Hi! Bonjour! Grüezi!</span><br/> */}
-                                        {/* <span id="my-name-is">I'm </span><br/> */}
                                         <span id="yannik">Yannik Büchi</span><br/>
                                         <img id="profile-pic" src={require("./Images/SemiProfessionalPicSmaller.jpg")} alt="Yannik"/>     
-                                        {/* <img id="personal-logo" src={require("./Images/logo.png")} alt="logo"/> */}
-                                        {/* <span id="i-am-a">a </span><br/> */}
                                         <div id="adjectives" className="content" style={{textAlign: "center"}}>
                                             <div className="content_container">
                                                 <ul className="content__container__list">
-                                                    <li className="content__container__list__item" style={{color: "lightblue", fontSize: "40px"}}>
+                                                    <li className="content__container__list__item" style={{color: "lightblue"}}>
                                                         Mission-driven
                                                     </li>
-                                                    <li className="content__container__list__item" style={{color: "lightblue", fontSize: "40px"}}>
+                                                    <li className="content__container__list__item" style={{color: "lightblue"}}>
                                                         Trilingual
                                                     </li>
-                                                    <li className="content__container__list__item" style={{color: "lightblue", fontSize: "40px"}}>
+                                                    <li className="content__container__list__item" style={{color: "lightblue"}}>
                                                         Fullstack
                                                     </li>
-                                                    <li className="content__container__list__item" style={{color: "lightblue", fontSize: "40px"}}>
+                                                    <li className="content__container__list__item" style={{color: "lightblue"}}>
                                                         Chocolate-loving
                                                     </li>
                                                 </ul>
                                             </div>
                                             <p id="software-engineer" style={{textAlign: "center", color: "white"}}>Software Engineer</p> 
-                                            <p id="skill-pane" style={{fontSize: "20px"}}>JavaScript | React | Ruby | Rails | SQL</p>
+                                            <p id="skill-pane">JavaScript | React | Ruby | Rails | SQL</p>
                                         </div>
                                     </div>
                                 </Col>
@@ -69,14 +63,18 @@ function Home(){
             <section id="home-content">
             
             <article id="featured" className="content-article">
-                <h2 className="heading-sec">Featured Project</h2>
+                
                 <Container >
                     <Row>
-                        <Col lg={3} sm={12}>
+                        <Col lg={12}>
+                            <h2 className="heading-sec">Featured Project</h2>
+                            <h3>Upskill your cooking. Become your own personal chef.</h3>
+
+                        </Col>
+                        <Col lg={12}>
                             <img className="floating" src={require("./Images/Cuisto.png")} style={{height: "200px"}} alt="Cuisto"/>
                         </Col>
-                        <Col lg={9} sm={12} className="vertically-center">
-                            <h3>Upskill your cooking. Become your own personal chef.</h3>
+                        <Col lg={12} className="vertically-center proj-nav" >
                             <Button href="https://cuisto.herokuapp.com/" target="_blank" className="nav-btn">Demo</Button>
                             <Button variant="secondary" className="nav-btn" href="/portfolio">View Full Portfolio</Button>
                         </Col>
@@ -89,10 +87,14 @@ function Home(){
                 <h2 className="heading-sec">Tool Kit</h2>
                         <Container>
                             
-                            <Row>
-                                <Col sm={12} md={6}>
+                            <Row >
+                                <Col className="front-end" lg={12}>
                                     <Row>
                                     <h4>Front-End</h4>
+                                    <Col sm={12}>
+                                        <img id="react" className="icon2" src={require("./Images/react2.png")} alt="javascript"/>
+                                        <p className="about-list">React</p>
+                                    </Col>
                                     <Col>
                                         <img className="icon2" src={require("./Images/JS.png")} alt="javascript"/>
                                         <p className="about-list">JavaScript</p>
@@ -105,22 +107,20 @@ function Home(){
                                         <img className="icon2" src={require("./Images/html2.png")} alt="javascript"/>
                                         <p className="about-list">HTML5</p>
                                     </Col>
-                                    <Col sm={12}>
-                                        <img className="icon2" src={require("./Images/react2.png")} alt="javascript"/>
-                                        <p className="about-list">React</p>
-                                    </Col>
+                                    
                                     </Row>
                                 </Col>
 
-                                <Col sm={12} md={6}>
+                                <Col className="front-end" lg={12} >
                                 <Row>
                                 <h4>Back-End</h4>
-                                <Col>
-                                    <img className="icon2"  src={require("./Images/Rb.png")} alt="ruby"/>
+                                <Col lg={12}>
+                                    <img id="ruby" className="icon2"  src={require("./Images/Rb.png")} alt="ruby"/>
                                     <p className="about-list">Ruby</p>
                                 </Col>
-                                <Col>
-                                    <img className="icon2"  src={require("./Images/rails2.png")} alt="ruby"/>
+                                <h2>On</h2>
+                                <Col lg={12}>
+                                    <img id="rails" className="icon2"  src={require("./Images/rails2.png")} alt="ruby"/>
                                     <p className="about-list">Rails</p>
                                 </Col>
                                 </Row>
@@ -155,7 +155,6 @@ function Home(){
                 
             </div> 
 
-            <AnimationOnScroll animateIn="animate__fadeIn">
                 <article className="page-text-container">
                     <Container>
                         <Row>
@@ -171,9 +170,8 @@ function Home(){
                         </Row>   
                     </Container> 
                 </article>
-            </AnimationOnScroll>
+
             
-            <AnimationOnScroll animateIn="animate__fadeIn">
                 <div className="page-text-container">
                     <Container>
                         <Row>
@@ -194,9 +192,9 @@ function Home(){
                         </Row>
                     </Container>
                 </div>
-            </AnimationOnScroll>
 
-            <AnimationOnScroll animateIn="animate__fadeIn">
+
+
                 <div className="page-text-container">
                     <Container>
                         <Row>
@@ -215,7 +213,6 @@ function Home(){
                         </Row>
                     </Container>
                 </div>
-            </AnimationOnScroll>
             </article>
             </section>
         </>
